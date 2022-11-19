786fbf5da66741d391ed03550c13b5c4a078e6ca
gio <wnsgyo9@gmail.com>
2022-11-19 11:48:13 +0900

    bcrypt jwt setting

--- server/router/user.js
+++ server/router/user.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import * as userController from '../controller/user.js';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
 
